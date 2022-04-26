dfcf9a7690873c0d8a915c5013d30f25d1b37b69
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-04-26 17:26:55 +0400

    First Commit

---
+++ P224Allup/DAL/AllupDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P224Allup.Models;
+
+namespace P224Allup.DAL
+{
+    public class AllupDbContext : DbContext
+    {
+        public AllupDbContext(DbContextOptions<AllupDbContext> options) : base(options){ }
+
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+    }
+}
